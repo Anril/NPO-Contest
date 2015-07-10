b73743878e3779e104cbe255e735477013d29060
Anril <as.lavrentev@ya.ru>
2015-07-10 19:01:26 +0400

    First commit

---
+++ Task 1/Task 1/Program.cs
@@ -0,0 +1,159 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public class Program
+    {
+
+        private enum SequanceState
+        { 
+            Rising,
+            Falling
+        }
+
+        private static void Main(string[] args)
+        {
+            int[][] matrix;
+
+            ReadMatrix("input.txt", out matrix);
+
+            if (matrix.Length > 0)
+            {
+                SortStrings(ref matrix);
+                TransposeMatrix(ref matrix);
+                SortStrings(ref matrix);
+                TransposeMatrix(ref matrix);
+            }
+
+            WriteMatrix("output.txt", matrix);
+        }
+
+        private static void ReadMatrix(string fileName, out int[][] matrix)
+        {
+            string text = System.IO.File.ReadAllText(fileName).Replace("\r\n", " ");
+            int[] values = text.Split(' ').Select(n => int.Parse(n)).ToArray();
+
+            CreateMatrix(out matrix, values[0], values[1]);
+
+            int curValue = 2;
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[0].Length; j++)
+                {
+                    matrix[i][j] = values[curValue];
+                    curValue++;
+                }
+            }
+        }
+
+        private static void CreateMatrix(out int [][] matrix, int heigth, int width)
+        {
+            matrix = new int[heigth][];
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[i] = new int[width];
+            }
+        }
+
+        private static void SortStrings(ref int[][] matrix)
+        {
+            int[] maxLengths = new int[matrix.GetLength(0)];
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                maxLengths[i] = FindMaxLength(matrix[i]);
+            }
+
+            for (int i = 0; i < maxLengths.Length; i++)
+            {
+                for (int j = i + 1; j < maxLengths.Length; j++)
+                {
+                    if (maxLengths[j] > maxLengths[i])
+                    {
+                        Swap<int>(ref maxLengths[i], ref maxLengths[j]);
+                        Swap<int[]>(ref matrix[i], ref matrix[j]);
+                    }
+                }
+            }
+        }
+
+        private static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+
+        private static int FindMaxLength(int[] array)
+        {
+            int maxLength = 2;
+            SequanceState curState, prevState;
+
+            if (array.Length > 2)
+            {
+                if (array[0] < array[1])
+                {
+                    curState = prevState = SequanceState.Rising;
+                }
+                else
+                {
+                    curState = prevState = SequanceState.Falling;
+                }
+
+                for (int i = 1; i < array.Length - 1; i++)
+                {
+                    if (array[i] < array[i + 1])
+                    {
+                        curState = SequanceState.Rising;
+                    }
+                    else
+                    {
+                        curState = SequanceState.Falling;
+                    }
+
+                    if (prevState == curState) maxLength++;
+
+                    prevState = curState;
+                }
+            }
+            return maxLength;
+        }
+
+        private static void TransposeMatrix(ref int[][] matrix)
+        {
+            int[][] newMatrix;
+            CreateMatrix(out newMatrix, matrix[0].Length, matrix.Length);
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[0].Length; j++)
+                {
+                    newMatrix[j][i] = matrix[i][j];
+                }
+            }
+
+            matrix = newMatrix;
+        }
+
+        private static void WriteMatrix(string fileName, int[][] matrix)
+        {
+            string[] lines = new string[matrix.Length];
+
+            for (int i = 0; i < lines.Length; i++)
+            { 
+                for (int j = 0; j < matrix[0].Length; j++)
+                {
+                    lines[i] += matrix[i][j] + (j < matrix[0].Length - 1 ? " ":"");
+                }
+            }
+
+            System.IO.File.WriteAllLines(fileName, lines);
+        }
+    }
+}
---
+++ Task 2/Task 2/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    public class Data
+    {
+        public int price;
+        public Node prevNode;
+    }
+
+    public class Node
+    {
+        public int name;
+        public List<Edge> edges;
+    }
+
+    public class Edge
+    {
+        public Node first;
+        public Node second;
+    }
+
+    public class Program
+    {
+        
+        private static void Main(string[] args)
+        {
+            Node startNode, endNode;
+            List<Node> listNodes = new List<Node>();
+            List<Edge> listEdges = new List<Edge>();
+
+            ReadFile("input.txt", listNodes, listEdges, out startNode, out endNode);
+
+            List<Node> minPath = FindShortPath(listNodes , listEdges, startNode, endNode);
+
+            WriteFile("output.txt", minPath);
+        }
+
+        private static void ReadFile(string fileName, List<Node> listNodes, 
+            List<Edge> listEdges,
+            out Node startNode, out Node endNode)
+        {
+            string text = System.IO.File.ReadAllText(fileName).Replace("\r\n", " ");
+            int[] values = text.Split(' ').Select(n => int.Parse(n)).ToArray();
+
+            int counNodes = values[0];
+
+            for (int i = 0; i < counNodes; i++)
+            {
+                listNodes.Add(new Node { name = i + 1 });
+            }
+
+            startNode = listNodes[values[1] - 1];
+            endNode = listNodes[values[2] - 1];
+
+            int curValue = 3;
+            for (int i = curValue; i <= values.Length-1; i++)
+            {
+                Edge tmp = new Edge();
+                tmp.first = listNodes[values[i] - 1];
+                i++;
+                tmp.second = listNodes[values[i] - 1]; ;
+                listEdges.Add(tmp);
+            }
+
+            for (int i = 0; i < listNodes.Count; i++)
+            {
+                listNodes[i].edges = new List<Edge>();
+                for (int j = 0; j < listEdges.Count; j++)
+                {
+                    if (listNodes[i] == listEdges[j].first ||
+                        listNodes[i] == listEdges[j].second)
+                    {
+                        listNodes[i].edges.Add(listEdges[j]);
+                    }
+                }
+            }
+
+        }
+
+        private static List<Node> FindShortPath(List<Node> listNodes, 
+            List<Edge> listEdges, Node startNode, Node endNode)
+        {
+            List<Node> notVisitedNodes = listNodes;
+            var track = new Dictionary<Node, Data>();
+            track.Add(startNode, new Data { prevNode = null, price = 0 });
+
+            while (true)
+            {
+                Node curNode = null;
+                int bestPrice = Int32.MaxValue;
+                foreach (Node node in notVisitedNodes)
+                {
+                    if (track.ContainsKey(node) && track[node].price <= bestPrice)
+                    {
+                        curNode = node;
+                        bestPrice = track[node].price;
+                    }
+                }
+
+                if (curNode == endNode) break;
+                if (curNode == null) return null;
+
+                foreach (var e in curNode.edges.Where(p => p.first == curNode))
+                {
+                    int curPrice = track[curNode].price + 1;
+                    var nextNode = e.second;
+                    if (!track.ContainsKey(nextNode) || track[nextNode].price > curPrice)
+                    {
+                        track[nextNode] = new Data { price = curPrice, prevNode = curNode };
+                    }
+                }
+                notVisitedNodes.Remove(curNode);
+            }
+
+            var result = new List<Node>();
+            while (endNode != null)
+            {
+                result.Add(endNode);
+                endNode = track[endNode].prevNode;
+            }
+            result.Reverse();
+
+            return result;
+        }
+
+        private static void WriteFile(string fileName, List<Node> minPath)
+        {
+            string line = String.Empty;
+            if (minPath != null)
+            {
+                foreach (var value in minPath)
+                {
+                    line += value.name.ToString() + " ";
+                }
+                System.IO.File.WriteAllText(fileName, line);
+            }
+        }
+    }
+}
